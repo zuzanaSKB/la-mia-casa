b110213e5d17504f1d48c47357dcb3c5f0c1ec1f
zuzanaSKB <zuzana.skb.ads@gmail.com>
2025-03-30 22:15:17 +0200

    add schema description

---
+++ my-app-be/tables.sql
@@ -0,0 +1,49 @@
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    phone_number VARCHAR(20),
+    password TEXT NOT NULL,
+    birth_date TIMESTAMP,
+    role VARCHAR(10) CHECK (role IN ('guest', 'admin')) NOT NULL
+);
+
+CREATE TABLE rooms (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price_per_night DECIMAL(5,2) NOT NULL,
+    capacity INTEGER CHECK (capacity > 1 AND capacity < 5) NOT NULL,
+    is_available BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE reservations (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    room_id INTEGER NOT NULL,
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    status VARCHAR(10) CHECK (status IN ('pending', 'confirmed', 'canceled')) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
+
+CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    room_id INTEGER NOT NULL,
+    text TEXT,
+    rating INTEGER CHECK (rating >= 1 AND rating <= 5) NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (room_id) REFERENCES rooms(id)
+);
+
+CREATE TABLE birthday_discounts (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER UNIQUE NOT NULL,
+    discount DECIMAL(5,2) CHECK (discount >= 0 AND discount <= 100) NOT NULL,
+    validity TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
