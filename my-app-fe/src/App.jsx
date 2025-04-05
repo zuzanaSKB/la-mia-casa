23a014f7c6d40eabb256c4645d4eec270b0e170d
zuzanaSKB <zuzana.skb.ads@gmail.com>
2025-04-05 13:26:33 +0200

    init dashboard page (frontend)

--- my-app-fe/src/App.jsx
+++ my-app-fe/src/App.jsx
@@ -1,17 +1,20 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./components/home";
 import Footer from "./components/footer";
+import Login from "./components/login";
+import Registration from "./components/registration";
+import Dashboard from "./components/dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Home />} />
-          <Route path="/registration" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
         <Footer />
       </div>
