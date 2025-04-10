ddc3cd1130273f11a0c562ae6b6d922de785c3a0
zuzanaSKB <zuzana.skb.ads@gmail.com>
2025-04-10 15:51:47 +0200

    init  dashboard for admin page

---
+++ my-app-fe/src/components/dashboardAdmin.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function DashboardAdmin(props) {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout()
+      .then(() => {
+          props.setAuthStatus(false);
+          navigate('/');
+      })
+      .catch((error) => {
+          console.log(error.message);
+          props.setError(error.message)
+      });
+  };
+
+  return (
+    <div
+      className="container-fluid"
+      style={{
+        minHeight: "100vh",
+        backgroundImage: "url('/watercolour painting-1.webp')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        position: "relative",
+      }}
+    >
+      <div style={{ position: "absolute", top: "10px", right: "10px" }}>
+        <button className="btn btn-danger btn-sm" onClick={handleLogout}>
+          Odhlásiť sa
+        </button>
+      </div>
+
+      <div
+        className="d-flex flex-column justify-content-center align-items-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div
+          className="dashboard-box p-4 text-white"
+          style={{
+            backgroundColor: "rgba(255, 255, 255, 0.2)",
+            borderRadius: "15px",
+            boxShadow: "0 4px 10px rgba(0, 0, 0, 0.3)",
+            backdropFilter: "blur(10px)",
+            width: "90%",
+            maxWidth: "800px",
+          }}
+        >
+          <h1 className="mb-4 text-center">Toto je stránka pre admina.</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardAdmin;
