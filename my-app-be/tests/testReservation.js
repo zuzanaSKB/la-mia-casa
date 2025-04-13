4c6ff5626aea3fea25ae1a7385dabf7eb32ec311
zuzanaSKB <zuzana.skb.ads@gmail.com>
2025-04-13 17:44:11 +0200

    test get available room function from db

--- my-app-be/tests/testReservation.js
+++ my-app-be/tests/testReservation.js
@@ -1,7 +1,9 @@
 import { addReservation } from '../models/reservations.js';
+import { getAvailableRooms } from '../models/reservations.js';
 
 async function test() {
-    const response = await addReservation(1, 1, "2025-04-01", "2025-04-05");
+    //const response = await addReservation(1, 1, "2025-04-01", "2025-04-05");
+    const response = await getAvailableRooms("2025-04-01", "2025-04-05");
     console.log(response);
 }
 
