7061fe3adaebb513c3ce716619d89466a54d7422
zuzanaSKB <zuzana.skb.ads@gmail.com>
2025-04-13 17:42:38 +0200

    add function for getting available rooms from db

--- my-app-be/models/reservations.js
+++ my-app-be/models/reservations.js
@@ -1,5 +1,39 @@
 import pool from '../config/db.js';
 
+export async function getAllRooms() {
+    try {
+        const result = await pool.query("SELECT * FROM rooms ORDER BY id");
+        return result.rows;
+      } catch (error) {
+        console.error("Error fetching rooms:", error);
+        throw new Error("Could not retrieve rooms from DB");
+      }
+};
+
+export async function getAvailableRooms(from, to) {
+    try {
+      const result = await pool.query(
+        `SELECT *
+         FROM rooms
+         WHERE is_available = true
+           AND id NOT IN (
+             SELECT room_id
+             FROM reservations
+             WHERE status IN ('confirmed', 'pending')
+               AND NOT (
+                 end_date <= $1 OR start_date >= $2
+               )
+           )
+         ORDER BY id`,
+        [from, to]
+      );
+      return result.rows;
+    } catch (err) {
+      console.error("Error checking room availability:", err);
+      throw new Error("Database error: could not fetch available rooms");
+    }
+  };
+
 export async function addReservation(user_id, room_id, start_date, end_date) {
     try {
         await pool.query(
