4ce912f78de4bf9c9c6e7ad32257d0043baec9d7
zuzanaSKB <zuzana.skb.ads@gmail.com>
2025-04-21 20:41:58 +0200

    edit var name

--- my-app-be/app.js
+++ my-app-be/app.js
@@ -30,10 +30,7 @@ app.use(cors({
   credentials: true
 }));
 
-if (process.env.STATUS === 'production') {
-  // trust proxy needed for secure cookie to work on render.com
-  // because render.com uses a reverse proxy to handle HTTPS requests
-  // and forwards the requests to the backend server over HTTP
+if (process.env.NODE_ENV === 'production') {
   app.set('trust proxy', 1);
 }
 
