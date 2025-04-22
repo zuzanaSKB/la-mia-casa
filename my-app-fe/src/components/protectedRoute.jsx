c0aaa130b4521a895dcf08a9845e124ab9f2ff3a
zuzanaSKB <zuzana.skb.ads@gmail.com>
2025-04-22 14:22:38 +0200

    edit navigate

--- my-app-fe/src/components/protectedRoute.jsx
+++ my-app-fe/src/components/protectedRoute.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 function ProtectedRoute({ authStatus, allowedRole, userRole, children }) {
-  //if not authenticated, redirect to home
+  //if not authenticated, redirect to login
   if (!authStatus) {
-    return <Navigate to="/" />;
+    return <Navigate to="/login" />;
   }
 
   //if user role doesn't match the allowed role, redirect to home
